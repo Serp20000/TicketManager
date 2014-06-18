1e479d28705704b8ec42e6b8ef76eb41e38eadf0
Vladislav Karpenko <vladislav@karpenko.od.ua>
2014-06-18 12:54:21 +0300

    add fill script

--- src/main/java/manager/controllers/WelcomeController.java
+++ src/main/java/manager/controllers/WelcomeController.java
@@ -1,6 +1,6 @@
 package manager.controllers;
 
-import manager.entities.*;
+import manager.entities.Ticket;
 import manager.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.util.Date;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -44,50 +43,51 @@ public class WelcomeController {
         //ВНИМАНИЕ! ЭТОТ КОД ДЛЯ ЗАПОЛНЕНИЯ БАЗЫ, ДЛЯ ЭТОГО НУЖНО УДАЛИТЬ БАЗУ ЧЕРЕЗ pgAdmin создать заново базу,
         //запустить программу и несклолько раз нажать refresh в бродилке
         //удалить отсюда
-        Operator operator = new Operator();
-        operator.setName("User");
-        operator.setSurname("Userovich");
-        operator.setLogin("user");
-        operator.setPassword("asdfqwerty");
-        operator.setDescription("first user");
-        operatorRepository.save(operator);
-
-        Client client = new Client();
-        client.setName("Client");
-        client.setDescription("");
-        clientRepository.save(client);
-
-
-        Event event = new Event();
-        event.setDate(new Date());
-        event.setDescription("football");
-        event.setOperator(operator);
-        eventRepository.save(event);
-
-        for (int i = 0; i < 28; i++) {
-            Sector zone = new Sector();
-            zone.setEvent(event);
-            zone.setName("Sector " + i);
-            zone.setMaxRows(20);
-            zone.setMaxSeats(50);
-            zone.setPrice(Double.valueOf(50 + i));
-            zone.setOperator(operator);
-            Ticket ticket = new Ticket();
-            ticket.setRow(10);
-            ticket.setSeat(15);
-            ticket.setSector(zone);
-            ticket.setOperator(operator);
-            ticket.setClient(client);
-            ticket.setReserved(true);
-            sectorRepository.save(zone);
-            ticketRepository.save(ticket);}
+//        Operator operator = new Operator();
+//        operator.setName("User");
+//        operator.setSurname("Userovich");
+//        operator.setLogin("user");
+//        operator.setPassword("asdfqwerty");
+//        operator.setDescription("first user");
+//        operatorRepository.save(operator);
+//
+//        Client client = new Client();
+//        client.setName("Client");
+//        client.setDescription("");
+//        clientRepository.save(client);
+//
+//
+//        Event event = new Event();
+//        event.setDate(new Date());
+//        event.setDescription("football");
+//        event.setOperator(operator);
+//        eventRepository.save(event);
+//
+//        for (int i = 0; i < 28; i++) {
+//            Sector zone = new Sector();
+//            zone.setEvent(event);
+//            zone.setName("Sector " + i);
+//            zone.setMaxRows(20);
+//            zone.setMaxSeats(50);
+//            zone.setPrice(Double.valueOf(50 + i));
+//            zone.setOperator(operator);
+//            Ticket ticket = new Ticket();
+//            ticket.setRow(10);
+//            ticket.setSeat(15);
+//            ticket.setSector(zone);
+//            ticket.setOperator(operator);
+//            ticket.setClient(client);
+//            ticket.setReserved(true);
+//            sectorRepository.save(zone);
+//            ticketRepository.save(ticket);}
+//
+        //удалить до сюда
 
         model.addAttribute("eventall", eventRepository.findAll());
         model.addAttribute("event", eventRepository.findAllByData());
         model.addAttribute("sector", sectorRepository.findAllByEvent_id(1));
         return "index";
     }
-    //удалить до сюда
 
     @RequestMapping(value="/buy_ticket", method = GET)
     public String newTicket(Model model) {
